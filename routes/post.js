3db8fd5ed2fd93f8c137c94e2e068b4390ea959f
sourabh.2021ca104@mnnit.ac.in <sourabh.2021ca104@mnnit.ac.in>
2022-10-31 21:31:40 +0530

    s

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Post = require('../models/post');
+const Post = require('../models/Post');
 
 // Create Post
 router.post("/create", async function (req, res) {
